4e0208869353a730f7b457ce687f27265926a1ac
Pedro Dias <pedroid199@gmail.com>
2021-09-01 17:02:18 -0300

    rota subtracao pronta

--- operacoes.js
+++ operacoes.js
@@ -7,6 +7,13 @@ router.post('/soma', (req, res) => {
     const soma = (num1*1) + (num2*1);
 
     return res.send({resultado: soma.toString()});
+});
+
+router.post('/subtracao', (req, res) => {
+    const {num1, num2} = req.body;
+    const subtracao = (num1*1) - (num2*1);
+
+    return res.send({resutado: subtracao.toString()});
 })
 
 module.exports = router;

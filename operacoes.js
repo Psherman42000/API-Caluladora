bc7b4957ecd91a06cd1f4a4897a7af4d13af36fe
Pedro Dias <pedroid199@gmail.com>
2021-09-02 16:20:10 -0300

    rota divisao pronta

--- operacoes.js
+++ operacoes.js
@@ -16,4 +16,11 @@ router.post('/subtracao', (req, res) => {
     return res.send({resutado: subtracao.toString()});
 })
 
+router.post('/divisao', (req, res) => {
+    const {num1, num2} = req.body;
+    const divisao = (num1*1)/(num2*1);
+
+    return res.send({resultado: divisao.toString()});
+})
+
 module.exports = router;

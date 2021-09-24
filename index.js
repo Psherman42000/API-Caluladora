12f586db9c24c60545100afc719b70eba8741f41
Pedro Dias <pedroid199@gmail.com>
2021-09-24 15:14:53 -0300

    versao refatorada

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
-app.use('/operacoes', router);
+app.use('/operacoes', router);;
 
 app.all('/', (req, res) => {
     return res.send({message: "ok"});
